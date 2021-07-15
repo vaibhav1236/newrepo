175ae71a26cd124a9b151ceb5368fdf70c065a4b
vaibhav1236 <87272843+vaibhav1236@users.noreply.github.com>
2021-07-15 19:56:47 +0530

    vaibhav

---
+++ css/script.js
@@ -0,0 +1,76 @@
+//  var foodcost =prompt("what is the total amount");
+//  var no=prompt("No of people");
+//  document.write("the total cost of food was ",foodcost,"<br>");
+//  document.write("Each one of you has to pay",(foodcost/no).toFixed(2),"<br>");
+
+
+
+// var arr=[];
+// var n=prompt("enter the no of bags")
+// for(x=0; x<=n;i=i=1);
+//     var x=prompt("weight")
+//     arr.push(x)
+
+// document.write(arr)
+
+// const arr = [];
+
+// for (i =0; i <= 3; i++) {
+//     var x = prompt("what is the weight");
+//     arr.push(x);
+
+// }
+// document.write(arr, "<br>");
+// sum = 0;parkale
+
+// for ( i in arr) {
+//     sum = sum + i;
+
+// }
+// document.write(sum)
+
+
+var name = prompt("enter the name of farmer ");
+document.write("name of the farmer is:", name, "<br>")
+
+// for (i=0; i<=4;i++)
+//     var weight=prompt("enter the weight of sugarcane" )
+// document.write("weight of sugarcane is :",weight,"<br>")
+
+
+var weight1 = prompt("enter the weight of day 1")
+document.write("weight of sugarcane is :", weight1, "<br>")
+var num1 =parseInt(weight1)
+
+var weight2 = prompt("enter the weight of day 2")
+document.write("weight of sugarcane is :", weight2, "<br>")
+var num2 =parseInt(weight2)
+
+var weight3 = prompt("enter the weight of day 3")
+document.write("weight of sugarcane is :", weight3, "<br>")
+var num3 =parseInt(weight3)
+
+var R=prompt("enter the rate of sugarcane")
+var num4=parseInt(R)
+
+document.write("total payment  of farmer is :", (num1 + num2 + num3)*num4)
+
+
+
+
+
+// const arr = [];
+
+// for (i = 0; i <= 3; i++) {
+//     var x = prompt("what is the weight");
+//     arr.push(x);
+
+// }
+// document.write(arr, "<br>");
+// sum = 0;
+
+// for (i in arr) {
+//     sum = sum + i;
+
+// }
+// document.write(int(sum))
